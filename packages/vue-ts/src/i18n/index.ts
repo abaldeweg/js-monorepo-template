129c748bbef1ba188d57577547b6deea8baf57b7
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-06-02 16:18:26 +0200

    Update index.ts

--- packages/vue-ts/src/i18n/index.ts
+++ packages/vue-ts/src/i18n/index.ts
@@ -1,20 +1,15 @@
 import { createI18n } from 'vue-i18n'
 
-const modules = import.meta.glob('./locales/*.json')
-
-// Function to extract the name from the file path
-const getName = (data: string): string => data.replace(/.\/locales\/(.*).json/, '$1')
-
-// Initialize an empty messages object to store the translations
-const messages: { [key: string]: any } = {}
-for (const path in modules) {
-  modules[path]().then((mod) => {
-    messages[getName(path)] = mod
-  })
+const loadLocale = async (locale: string) => {
+  const messages = await import(`./locales/${locale}.json`)
+  return messages.default
 }
 
-// Create and export the i18n instance with the specified options
-// Use camelCase or snake_case as key names, kebab-case could lead into issues
+const messages = {
+  en: await loadLocale('en'),
+  de: await loadLocale('de'),
+}
+
 export default createI18n({
   legacy: false,
   locale: navigator.language,
